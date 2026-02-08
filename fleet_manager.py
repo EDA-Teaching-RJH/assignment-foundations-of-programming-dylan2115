c4996bc1e2849bae9a69559e1fb633628d104f70
dylan lewis <djl51@kent.ac.uk>
2026-02-08 23:49:46 +0000

    v1

---
+++ fleet_manager.py
@@ -0,0 +1,8 @@
+def init_database():
+
+name = ["picard", "riker", "data", "forge", "crusher"]
+rank = ["captain", "comander", "lt. comander", "lieutenant", "comander"]
+division = ["command", "command ", "operations","operatioms", "sciences"]
+ids = [1001, 1002, 1003, 1004, 1005] 
+
+return name, rank, division, ids
