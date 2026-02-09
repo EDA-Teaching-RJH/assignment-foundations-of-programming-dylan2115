eec3e5e9bf2586ca45c2e570a46262414fee12e4
dylan lewis <djl51@kent.ac.uk>
2026-02-09 17:24:30 +0000

    v3

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,8 +1,10 @@
 def init_database():
 
-title = ["picard", "riker", "data", "forge", "crusher"]
-rank = ["captain", "comander", "lt. comander", "lieutenant", "comander"]
-division = ["command", "command ", "operations","operatioms", "sciences"]
-ids = [1001, 1002, 1003, 1004, 1005] 
+    title = ["picard", "riker", "data", "forge", "crusher"]
+    rank = ["captain", "comander", "lt. comander", "lieutenant", "comander"]
+    division = ["command", "command ", "operations","operatioms", "sciences"]
+    ids = [1001, 1002, 1003, 1004, 1005] 
 
-return title, rank, division, ids
+    return title, rank, division, ids
+
+
